c0409038d7cf5b22055c892881594f2992e86286
Maizil <142029402+Maizil41@users.noreply.github.com>
2025-06-12 05:01:16 +0700

    install

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+wget -O /tmp/Bahan.zip https://github.com/Maizil41/Mutiara-Wrt/raw/beta/Bahan.zip
+
+unzip /tmp/Bahan.zip -d /tmp/
+
+mv /tmp/Bahan/whatsapp /usr/lib/lua/luci/view/
+
+rm -f /www/luci-static/resources/view/whatsapp/whatsapp.js
+
+mv /tmp/Bahan/whatsapp.js /www/luci-static/resources/view/whatsapp/
+
+rm -f /usr/share/luci/menu.d/luci-app-whatsapp-bot.json
+
+mv /tmp/Bahan/luci-app-whatsapp-bot.json /usr/share/luci/menu.d/
+
+rm -f /tmp/Bahan.zip
+rm -rf /tmp/Bahan
